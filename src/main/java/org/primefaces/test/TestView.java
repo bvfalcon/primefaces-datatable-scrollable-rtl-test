cebb030e09043ac367e3a34f59a74089e803c568
Vladimir V. Bychkov <github@bychkov.name>
2024-01-25 00:37:37 +0100

    Demo example

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -17,15 +17,10 @@ import lombok.Data;
 @ViewScoped
 public class TestView implements Serializable {
     
-    private String string;
-    private Integer integer;
-    private BigDecimal decimal;
-    private LocalDateTime localDateTime;
     private List<TestObject> list;
     
     @PostConstruct  
     public void init() {
-        string = "Welcome to PrimeFaces!!!";
         list = new ArrayList<>(Arrays.asList(
                 new TestObject("Thriller", "Michael Jackson", 1982),
                 new TestObject("Back in Black", "AC/DC", 1980),
